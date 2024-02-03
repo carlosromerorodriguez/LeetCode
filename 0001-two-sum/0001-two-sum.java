cb6ebd8c3ffebec22a4a48eb4e64e7e2843105f6
Carlos Romero <119690391+carlosromerorodriguez@users.noreply.github.com>
2024-02-03 14:33:45 +0100

    Time: 2 ms (94.06%), Space: 45 MB (17.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,15 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        if (nums.length <= 1) return nums;
+        HashMap<Integer, Integer> complements = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target && i != j) {
-                    return new int[]{j, i};
-                }
+            Integer newComplementIdx = complements.get(nums[i]);
+            if (newComplementIdx != null) {
+                return new int[]{newComplementIdx, i};
             }
+            complements.put(target - nums[i], i);
         }
 
         return nums;
